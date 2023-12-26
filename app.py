6f7b1625622ca7bde65140eea6fd7fa2b77d014b
Ev <maxa280391@gmail.com>
2023-12-26 13:56:00 -0500

    diable local run

--- app.py
+++ app.py
@@ -101,5 +101,5 @@ def get_list():
 def home():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    app.run(debug=True)
